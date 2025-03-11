fa1f012b910dc6ba4f1e79e66ed5718402143671
Clémence <153609756+clemence6565217@users.noreply.github.com>
2025-03-11 09:34:11 +0100

    Update latence.py

--- Tests performances/latence.py
+++ Tests performances/latence.py
@@ -2,7 +2,6 @@
 # TEST DE LATENCE
 # Le test de latence mesure le temps de réponse du switch entre l'envoi et la réception des paquets ICMP (ping) pour évaluer la performance du réseau.
 
-
 from scapy.all import *
 import time
 
