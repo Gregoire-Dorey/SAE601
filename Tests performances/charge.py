a8d7636940982f5ee8533197a358cec64045659c
clemence6565217 <clemence.charles@outlook.be>
2025-03-13 08:51:23 +0100

    update import

--- Tests performances/charge.py
+++ Tests performances/charge.py
@@ -3,6 +3,7 @@
 # Les deux PC (192.168.1.1 et 192.168.1.2) sont connectés aux ports du switch pour permettre la communication et tester la charge réseau via le switch (192.168.1.254).
 
 from scapy.all import *
+from scapy.layers.inet import IP, ICMP
 
 # Adresse IP cible
 target_ip = "192.168.111.175"
