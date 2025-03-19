9a3955826425ba816abeb03217a0f191a83c2d98
Gregoire-Dorey <74415996+Gregoire-Dorey@users.noreply.github.com>
2025-03-19 14:00:41 +0100

    Add files via upload

---
+++ FINAL/utils/graph.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+
+def bargraph(data,nom="",nom_x="",nom_y=""):
+    x=[]
+    y=[]
+    for i in data:
+        x.append(i[0])
+        y.append(i[1])
+
+    # Création du diagramme en barres
+    plt.figure(figsize=(8, 5))
+    plt.bar(x, y, color='dodgerblue')
+
+    # Ajouter des labels et un titre
+    plt.xlabel(nom_x)
+    plt.ylabel(nom_y)
+    plt.title(nom)
+
+    # Afficher le graphique
+    plt.show()
+
