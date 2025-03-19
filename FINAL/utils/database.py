8360a3947a8c510f52ea6c62ec46abeb241314e5
clemence6565217 <clemence.charles@outlook.be>
2025-03-19 14:32:03 +0100

    normally it's ok!

--- FINAL/utils/database.py
+++ FINAL/utils/database.py
@@ -1,17 +1,18 @@
 import sqlite3
-def insert_in_base(sw_name,latency):
+def insert_in_base(sw_name,latency,table):
     conn = sqlite3.connect("./utils/db-test.db")
     curs = conn.cursor()
-    curs.execute("INSERT INTO latence (name,latence) VALUES (?,?)",(sw_name,latency))
+    curs.execute(f"INSERT INTO {table} (name,latence) VALUES (?,?);",(sw_name,latency))
     conn.commit()
     conn.close()
 
-def select_latency():
+def select_latency(table):
     conn = sqlite3.connect("./utils/db-test.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT name, latence FROM latence")
+    cursor.execute(f"SELECT name, latence FROM {table};")
     data = cursor.fetchall()
     conn.close()
     return data
+
 
 print(select_latency())
