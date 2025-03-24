e96edb049dc24d68a50dc14fcc0bcdf991648a9a
adminetu <clemence.charles@etu.unicaen.fr>
2025-03-24 09:48:30 +0100

    Add insert_in_base_router function to database.py
    This function enables inserting router metrics into a specified table in the SQLite database. It ensures type conversion for inputs and includes timestamping for each entry.

--- final/database.py
+++ final/database.py
@@ -26,4 +26,19 @@ def select_latency(table):
     return data
 
 
+def insert_in_base_router(router_name,latency,table):
+    if type(router_name)!=str:
+        sw_name = str(router_name)
+    if type(latency)!=str:
+        latency = str(latency)
+    if type(table)!=str:
+        table = str(table)
+    date = dt.datetime.now()
+    date = date.strftime("%d/%m/%Y-%H:%M:%S")
+    conn = sqlite3.connect("C:/Users/adminetu/PycharmProjects/SAE601/final/db-metrics.db")
+    curs = conn.cursor()
+    curs.execute(f"INSERT INTO {table} (switch_name,latence,date) VALUES (?,?,?);",(router_name,latency,date))
+    conn.commit()
+    conn.close()
+
 #print(select_latency())
